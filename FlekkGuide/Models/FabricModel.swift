9fa0b56e1dc0566b2545aecba50c1d78f0121f03
Ingrid Skoge <insk034@student.kristiania.no>
2026-04-22 13:26:49 +0200

    opprettet database og lagt inn info

---
+++ FlekkGuide/Models/FabricModel.swift
@@ -0,0 +1,18 @@
+//
+//  FabricModel.swift
+//  FlekkGuide
+//
+//  Created by Ingrid Skoge on 22/04/2026.
+//
+
+import SwiftUI
+
+struct FabricModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FabricModel()
+}
---
+++ FlekkGuide/Models/StainModel.swift
@@ -0,0 +1,18 @@
+//
+//  StainModel.swift
+//  FlekkGuide
+//
+//  Created by Ingrid Skoge on 22/04/2026.
+//
+
+import SwiftUI
+
+struct StainModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    StainModel()
+}
